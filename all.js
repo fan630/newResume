628eb6b2ad9a3cd7e50702e34c07b2155583facd
fan630 <48875309+fan630@users.noreply.github.com>
2020-03-28 20:40:21 +0800

    revise font-style

--- all.js
+++ all.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
+    
     $(window).resize(function () {
         wdth = $(window).width();
         if(wdth > 767){
             $('.js-show').addClass('show')
         }
     }); 
+
+
+
 });
 
+
+$('#mySwitch').on('switch-change', function (e, data) {
+    var $el = $(data.el)
+        , value = data.value;
+    console.log(e, $el, value);
+});
 
